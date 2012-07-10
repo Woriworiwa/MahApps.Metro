12bcec9d5b868f312bd7a5f6a6dda2165742b2dd
sdf <sdf>
2012-07-10 22:08:07 +0200

    Firs Attempt to commit on master branch on the forked project

---
+++ Demo/BarButton.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demo
+{
+	/// <summary>
+	/// Interaction logic for BarButton.xaml
+	/// </summary>
+	public partial class BarButton : UserControl
+	{
+		public BarButton()
+		{
+			this.InitializeComponent();            
+		}
+
+        public static readonly DependencyProperty MetroImageSourceProperty =
+            DependencyProperty.Register("MetroImageSource", typeof(Visual), typeof(BarButton), new PropertyMetadata(default(Visual)));
+
+        public Visual MetroImageSource
+        {           
+            get { return (Visual)GetValue(MetroImageSourceProperty); }
+            set { SetValue(MetroImageSourceProperty, value); }
+        }
+	}
+}
---
+++ MahApps.Metro/Controls/MenuButton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Media;
+
+namespace MahApps.Metro.Controls
+{
+    public class MenuButton: Button
+    {
+        public MenuButton()
+        {
+            DefaultStyleKey = typeof(AppBarButton);
+        }
+
+        public static readonly DependencyProperty MetroImageSourceProperty =
+            DependencyProperty.Register("MetroImageSource", typeof(Visual), typeof(AppBarButton), new PropertyMetadata(default(Visual)));
+
+        public Visual MetroImageSource
+        {
+            get { return (Visual)GetValue(MetroImageSourceProperty); }
+            set { SetValue(MetroImageSourceProperty, value); }
+        }
+    }
+}
