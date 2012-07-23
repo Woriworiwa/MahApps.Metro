d4c643648ee761963b8f440e99cc389b1504c4f4
sdf <sdf>
2012-07-23 22:20:13 +0200

    About screen

--- Demo/NiceControlStation.xaml.cs
+++ Demo/NiceControlStation.xaml.cs
@@ -44,13 +44,60 @@ namespace Demo
             {
                 DoubleAnimation positionAnimation = new DoubleAnimation();
                 positionAnimation.From = (double)solution_pointer.GetValue(Canvas.TopProperty);
-                positionAnimation.To = (double)(solutions.SelectedIndex * (10 + 35) + (10 + 35/2 - 9) );
+                positionAnimation.To = (double)(solutions.SelectedIndex * (10 + 35) + (10 + 35 / 2 - 9));
                 positionAnimation.Duration = TimeSpan.FromMilliseconds(150);
                 solution_pointer.BeginAnimation(Canvas.TopProperty, positionAnimation);
             }
 
-            triggers_tab.SelectedIndex = solutions.SelectedIndex;
-                //solution_pointer.SetValue(Canvas.TopProperty, (double)(solutions.SelectedIndex * 55));
+            try
+            {
+                triggers_tab.SelectedIndex = solutions.SelectedIndex;
+            }
+            catch (Exception)
+            {
+
+            }
+            //solution_pointer.SetValue(Canvas.TopProperty, (double)(solutions.SelectedIndex * 55));
+        }
+
+        private void TextBlock_MouseDown_1(object sender, MouseButtonEventArgs e)
+        {
+            content_tab.SelectedIndex = 0;
+
+            about_button.FontWeight = FontWeights.Bold;
+            about_button.Foreground = Brushes.Black;
+
+            triggers_button.FontWeight = FontWeights.Normal;
+            triggers_button.Foreground = Brushes.Gray;
+
+            logs_button.FontWeight = FontWeights.Normal;
+            logs_button.Foreground = Brushes.Gray;
+        }
+
+        private void TextBlock_MouseDown_2(object sender, MouseButtonEventArgs e)
+        {
+            content_tab.SelectedIndex = 1;
+            about_button.FontWeight = FontWeights.Normal;
+            about_button.Foreground = Brushes.Gray;
+
+            triggers_button.FontWeight = FontWeights.Bold;
+            triggers_button.Foreground = Brushes.Black;
+
+            logs_button.FontWeight = FontWeights.Normal;
+            logs_button.Foreground = Brushes.Gray;
+        }
+
+        private void TextBlock_MouseDown_3(object sender, MouseButtonEventArgs e)
+        {
+            content_tab.SelectedIndex = 2;
+            about_button.FontWeight = FontWeights.Normal;
+            about_button.Foreground = Brushes.Gray;
+
+            triggers_button.FontWeight = FontWeights.Normal;
+            triggers_button.Foreground = Brushes.Gray;
+
+            logs_button.FontWeight = FontWeights.Bold;
+            logs_button.Foreground = Brushes.Black;
         }
     }
 }
